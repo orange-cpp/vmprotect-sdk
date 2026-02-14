c902751422f10473298b166d76f3c63718fc24ac
orange <orange-cpp@yandex.ru>
2026-02-14 07:59:54 +0300

    added standard spec

--- source/VMProtectSDK.cpp
+++ source/VMProtectSDK.cpp
@@ -115,32 +115,35 @@ int VMP_API VMProtectGetOfflineDeactivationString(const char*, char*, int)
     return ACTIVATION_OK;
 }
 
-#ifdef __APPLE__
-unsigned long GetTickCount()
+namespace
 {
-    const int64_t one_million = 1000 * 1000;
-    mach_timebase_info_data_t timebase_info;
-    mach_timebase_info(&timebase_info);
+#ifdef __APPLE__
+    unsigned long GetTickCount()
+    {
+        const int64_t one_million = 1000 * 1000;
+        mach_timebase_info_data_t timebase_info;
+        mach_timebase_info(&timebase_info);
 
-    // mach_absolute_time() returns billionth of seconds,
-    // so divide by one million to get milliseconds
-    return static_cast<uint32_t>((mach_absolute_time() * timebase_info.numer) / (one_million * timebase_info.denom));
-}
+        // mach_absolute_time() returns billionth of seconds,
+        // so divide by one million to get milliseconds
+        return static_cast<uint32_t>((mach_absolute_time() * timebase_info.numer) / (
+                                         one_million * timebase_info.denom));
+    }
 #endif
 
 #ifdef __unix__
-unsigned long GetTickCount()
-{
-    timeval tv;
-    gettimeofday(&tv, nullptr);
-    return tv.tv_sec * 1000 + tv.tv_usec / 1000;
-}
+    unsigned long GetTickCount()
+    {
+        timeval tv;
+        gettimeofday(&tv, nullptr);
+        return tv.tv_sec * 1000 + tv.tv_usec / 1000;
+    }
 #endif
 
 #ifndef WIN_DRIVER
-bool g_serial_is_correct = false;
-bool g_serial_is_blacklisted = false;
-uint32_t g_time_of_start = GetTickCount();
+    bool g_serial_is_correct = false;
+    bool g_serial_is_blacklisted = false;
+    uint32_t g_time_of_start = GetTickCount();
 #endif
 
 #ifdef VMP_GNU
@@ -148,246 +151,248 @@ uint32_t g_time_of_start = GetTickCount();
 #define strcmpi strcasecmp
 #define INI_MAX_LINE 1024
 
-size_t strnlen(char* text, const size_t max_len)
-{
-    const auto last = static_cast<const char*>(memchr(text, '\0', max_len));
-    return last ? static_cast<size_t>(last - text) : max_len;
-}
-
-/* Strip whitespace chars off end of given string, in place. Return s. */
-static char* rstrip(char* s)
-{
-    char* p = s + strlen(s);
-    while (p > s && isspace(static_cast<unsigned char>(*--p)))
-        *p = '\0';
-    return s;
-}
-
-/* Return pointer to first non-whitespace char in given string. */
-static char* lskip(const char* s)
-{
-    while (*s && isspace(static_cast<unsigned char>(*s)))
-        s++;
-    return const_cast<char*>(s);
-}
-
-/* Return pointer to first char c or ';' comment in given string, or pointer to
-   null at end of string if neither found. ';' must be prefixed by a whitespace
-   character to register as a comment. */
-static char* find_char_or_comment(const char* s, char c)
-{
-    int was_whitespace = 0;
-    while (*s && *s != c && !(was_whitespace && *s == ';'))
+    size_t strnlen(char* text, const size_t max_len)
     {
-        was_whitespace = isspace(static_cast<unsigned char>(*s));
-        s++;
+        const auto last = static_cast<const char*>(memchr(text, '\0', max_len));
+        return last ? static_cast<size_t>(last - text) : max_len;
     }
-    return const_cast<char*>(s);
-}
 
-/* See documentation in header file. */
-static int GetPrivateProfileString(const char* section_name, const char* key_name, char* buffer, size_t size,
-                                   const char* file_name)
-{
-    if (!buffer || !size)
-        return 0;
-
-    FILE* file = fopen(file_name, "r");
-    if (!file)
-        return 0;
-
-    char line[INI_MAX_LINE];
-    char* end;
-    int lineno = 0;
-    int res = 0;
-    bool section_found = false;
-
-    /* Scan through file line by line */
-    while (fgets(line, INI_MAX_LINE, file) != NULL)
+    /* Strip whitespace chars off end of given string, in place. Return s. */
+    char* rstrip(char* s)
     {
-        lineno++;
+        char* p = s + strlen(s);
+        while (p > s && isspace(static_cast<unsigned char>(*--p)))
+            *p = '\0';
+        return s;
+    }
 
-        char *start = line;
-        if (lineno == 1 && static_cast<unsigned char>(start[0]) == 0xEF &&
-            static_cast<unsigned char>(start[1]) == 0xBB &&
-            static_cast<unsigned char>(start[2]) == 0xBF)
-        {
-            start += 3;
-        }
-        start = lskip(rstrip(start));
+    /* Return pointer to first non-whitespace char in given string. */
+    char* lskip(const char* s)
+    {
+        while (*s && isspace(static_cast<unsigned char>(*s)))
+            s++;
+        return const_cast<char*>(s);
+    }
 
-        if (*start == ';' || *start == '#')
+    /* Return pointer to first char c or ';' comment in given string, or pointer to
+       null at end of string if neither found. ';' must be prefixed by a whitespace
+       character to register as a comment. */
+    char* find_char_or_comment(const char* s, char c)
+    {
+        int was_whitespace = 0;
+        while (*s && *s != c && !(was_whitespace && *s == ';'))
         {
-            /* Per Python ConfigParser, allow '#' comments at start of line */
+            was_whitespace = isspace(static_cast<unsigned char>(*s));
+            s++;
         }
-        else if (*start == '[')
+        return const_cast<char*>(s);
+    }
+
+    /* See documentation in header file. */
+    int GetPrivateProfileString(const char* section_name, const char* key_name, char* buffer, size_t size,
+                                const char* file_name)
+    {
+        if (!buffer || !size)
+            return 0;
+
+        FILE* file = fopen(file_name, "r");
+        if (!file)
+            return 0;
+
+        char line[INI_MAX_LINE];
+        char* end;
+        int lineno = 0;
+        int res = 0;
+        bool section_found = false;
+
+        /* Scan through file line by line */
+        while (fgets(line, INI_MAX_LINE, file) != NULL)
         {
-            /* A "[section]" line */
-            end = find_char_or_comment(start + 1, ']');
-            if (*end == ']')
+            lineno++;
+
+            char* start = line;
+            if (lineno == 1 && static_cast<unsigned char>(start[0]) == 0xEF &&
+                static_cast<unsigned char>(start[1]) == 0xBB &&
+                static_cast<unsigned char>(start[2]) == 0xBF)
             {
-                *end = '\0';
-                if (section_found)
-                    break;
-
-                section_found = strcmpi(start + 1, section_name) == 0;
+                start += 3;
             }
-        }
-        else if (section_found && *start && *start != ';')
-        {
-            /* Not a comment, must be a name[=:]value pair */
-            end = find_char_or_comment(start, '=');
-            if (*end != '=')
-            {
-                end = find_char_or_comment(start, ':');
-            }
-            if (*end == '=' || *end == ':')
-            {
-                *end = '\0';
-                char *name = rstrip(start);
-                char *value = lskip(end + 1);
-                end = find_char_or_comment(value, '\0');
-                if (*end == ';')
-                    *end = '\0';
-                rstrip(value);
+            start = lskip(rstrip(start));
 
-                if (strcmpi(name, key_name) == 0)
+            if (*start == ';' || *start == '#')
+            {
+                /* Per Python ConfigParser, allow '#' comments at start of line */
+            }
+            else if (*start == '[')
+            {
+                /* A "[section]" line */
+                end = find_char_or_comment(start + 1, ']');
+                if (*end == ']')
                 {
-                    strncpy(buffer, value, size);
-                    res = strnlen(buffer, size);
-                    break;
+                    *end = '\0';
+                    if (section_found)
+                        break;
+
+                    section_found = strcmpi(start + 1, section_name) == 0;
+                }
+            }
+            else if (section_found && *start && *start != ';')
+            {
+                /* Not a comment, must be a name[=:]value pair */
+                end = find_char_or_comment(start, '=');
+                if (*end != '=')
+                {
+                    end = find_char_or_comment(start, ':');
+                }
+                if (*end == '=' || *end == ':')
+                {
+                    *end = '\0';
+                    char* name = rstrip(start);
+                    char* value = lskip(end + 1);
+                    end = find_char_or_comment(value, '\0');
+                    if (*end == ';')
+                        *end = '\0';
+                    rstrip(value);
+
+                    if (strcmpi(name, key_name) == 0)
+                    {
+                        strncpy(buffer, value, size);
+                        res = strnlen(buffer, size);
+                        break;
+                    }
                 }
             }
         }
+
+        fclose(file);
+        return res;
     }
 
-    fclose(file);
-    return res;
-}
-
-static bool GetIniValue(const char* value_name, char* buffer, size_t size)
-{
-    char file_name[PATH_MAX];
-    file_name[0] = 0;
-    uint32_t name_size = sizeof(file_name);
+    bool GetIniValue(const char* value_name, char* buffer, size_t size)
+    {
+        char file_name[PATH_MAX];
+        file_name[0] = 0;
+        uint32_t name_size = sizeof(file_name);
 #if defined(__APPLE__)
-    _NSGetExecutablePath(file_name, &name_size);
+        _NSGetExecutablePath(file_name, &name_size);
 #else
-    int sz = readlink("/proc/self/exe", file_name, name_size);
-    if (sz > 0)
-        file_name[sz] = 0;
+        int sz = readlink("/proc/self/exe", file_name, name_size);
+        if (sz > 0)
+            file_name[sz] = 0;
 #endif
-    char* p = strrchr(file_name, '/');
-    if (p)
-        *(p + 1) = 0;
-    strncat(file_name, "VMProtectLicense.ini", sizeof(file_name) - (p - file_name));
-    return GetPrivateProfileString("TestLicense", value_name, buffer, size, file_name) != 0;
-}
-
-constexpr  std::array<uint8_t, 5> utf8_limits = {0xC0, 0xE0, 0xF0, 0xF8, 0xFC};
-
-static void ConvertUTF8ToUnicode(const uint8_t* src, size_t len, VMP_WCHAR* dest, size_t dest_size)
-{
-    if (!dest || dest_size == 0)
-        return; // nothing to do
-
-    size_t pos = 0;
-    size_t dest_pos = 0;
-    while (pos < len && dest_pos < dest_size)
-    {
-        uint8_t b = src[pos++];
-
-        if (b < 0x80)
-        {
-            dest[dest_pos++] = b;
-            continue;
-        }
-
-        size_t val_len;
-        for (val_len = 0; val_len <= utf8_limits.size(); val_len++)
-        {
-            if (b < utf8_limits[val_len])
-                break;
-        }
-
-        if (val_len == 0)
-            continue;
-
-        uint32_t value = b - utf8_limits[val_len - 1];
-        for (size_t i = 0; i < val_len; i++)
-        {
-            if (pos == len)
-                break;
-            b = src[pos++];
-            if (b < 0x80 || b >= 0xC0)
-                break;
-            value <<= 6;
-            value |= (b - 0x80);
-        }
-
-        if (value < 0x10000)
-        {
-            dest[dest_pos++] = static_cast<uint16_t>(value);
-        }
-        else if (value <= 0x10FFFF)
-        {
-            value -= 0x10000;
-            dest[dest_pos++] = static_cast<uint16_t>(0xD800 + (value >> 10));
-            dest[dest_pos++] = static_cast<uint16_t>(0xDC00 + (value & 0x3FF));
-        }
+        char* p = strrchr(file_name, '/');
+        if (p)
+            *(p + 1) = 0;
+        strncat(file_name, "VMProtectLicense.ini", sizeof(file_name) - (p - file_name));
+        return GetPrivateProfileString("TestLicense", value_name, buffer, size, file_name) != 0;
     }
 
-    if (dest_pos < dest_size - 1)
-        dest[dest_pos] = 0;
-    else
-        dest[dest_pos - 1] = 0;
-}
 
-static bool GetIniValue(const char* value_name, VMP_WCHAR* buffer, size_t size)
-{
-    char value[INI_MAX_LINE];
-    if (GetIniValue(value_name, value, sizeof(value)))
+    void ConvertUTF8ToUnicode(const uint8_t* src, size_t len, VMP_WCHAR* dest, size_t dest_size)
     {
-        ConvertUTF8ToUnicode(reinterpret_cast<uint8_t*>(value), strlen(value), buffer, size);
-        return true;
+        if (!dest || dest_size == 0)
+            return; // nothing to do
+
+        size_t pos = 0;
+        size_t dest_pos = 0;
+        while (pos < len && dest_pos < dest_size)
+        {
+            constexpr std::array<uint8_t, 5> utf8_limits = {0xC0, 0xE0, 0xF0, 0xF8, 0xFC};
+            uint8_t b = src[pos++];
+
+            if (b < 0x80)
+            {
+                dest[dest_pos++] = b;
+                continue;
+            }
+
+            size_t val_len;
+            for (val_len = 0; val_len <= utf8_limits.size(); val_len++)
+            {
+                if (b < utf8_limits[val_len])
+                    break;
+            }
+
+            if (val_len == 0)
+                continue;
+
+            uint32_t value = b - utf8_limits[val_len - 1];
+            for (size_t i = 0; i < val_len; i++)
+            {
+                if (pos == len)
+                    break;
+                b = src[pos++];
+                if (b < 0x80 || b >= 0xC0)
+                    break;
+                value <<= 6;
+                value |= (b - 0x80);
+            }
+
+            if (value < 0x10000)
+            {
+                dest[dest_pos++] = static_cast<uint16_t>(value);
+            }
+            else if (value <= 0x10FFFF)
+            {
+                value -= 0x10000;
+                dest[dest_pos++] = static_cast<uint16_t>(0xD800 + (value >> 10));
+                dest[dest_pos++] = static_cast<uint16_t>(0xDC00 + (value & 0x3FF));
+            }
+        }
+
+        if (dest_pos < dest_size - 1)
+            dest[dest_pos] = 0;
+        else
+            dest[dest_pos - 1] = 0;
     }
-    if (buffer && size)
-        buffer[0] = 0;
-    return false;
-}
+
+    bool GetIniValue(const char* value_name, VMP_WCHAR* buffer, size_t size)
+    {
+        char value[INI_MAX_LINE];
+        if (GetIniValue(value_name, value, sizeof(value)))
+        {
+            ConvertUTF8ToUnicode(reinterpret_cast<uint8_t*>(value), strlen(value), buffer, size);
+            return true;
+        }
+        if (buffer && size)
+            buffer[0] = 0;
+        return false;
+    }
 
 #elif defined(WIN_DRIVER)
 #else
 
-static bool GetIniValue(const char* value_name, wchar_t* buffer, size_t size)
-{
-    wchar_t file_name[MAX_PATH];
-    file_name[0] = 0;
-    GetModuleFileNameW(NULL, file_name, _countof(file_name));
-    wchar_t* p = wcsrchr(file_name, L'\\');
-    if (p)
-        *(p + 1) = 0;
-    wcsncat_s(file_name, L"VMProtectLicense.ini", _countof(file_name));
-
-    wchar_t key_name[1024] = {0};
-    MultiByteToWideChar(CP_ACP, 0, value_name, -1, key_name, _countof(key_name));
-    return GetPrivateProfileStringW(L"TestLicense", key_name, L"", buffer, static_cast<DWORD>(size), file_name) != 0;
-}
-
-static bool GetIniValue(const char* value_name, char* buffer, size_t size)
-{
-    wchar_t value[2048];
-    if (GetIniValue(value_name, value, sizeof(value)))
+    bool GetIniValue(const char* value_name, wchar_t* buffer, size_t size)
     {
-        WideCharToMultiByte(CP_ACP, 0, value, -1, buffer, static_cast<int>(size), NULL, NULL);
-        return true;
+        wchar_t file_name[MAX_PATH];
+        file_name[0] = 0;
+        GetModuleFileNameW(NULL, file_name, _countof(file_name));
+        wchar_t* p = wcsrchr(file_name, L'\\');
+        if (p)
+            *(p + 1) = 0;
+        wcsncat_s(file_name, L"VMProtectLicense.ini", _countof(file_name));
+
+        wchar_t key_name[1024] = {0};
+        MultiByteToWideChar(CP_ACP, 0, value_name, -1, key_name, _countof(key_name));
+        return GetPrivateProfileStringW(L"TestLicense", key_name, L"", buffer, static_cast<DWORD>(size), file_name) !=
+               0;
     }
-    if (buffer && size)
-        buffer[0] = 0;
-    return false;
-}
+
+    bool GetIniValue(const char* value_name, char* buffer, size_t size)
+    {
+        wchar_t value[2048];
+        if (GetIniValue(value_name, value, sizeof(value)))
+        {
+            WideCharToMultiByte(CP_ACP, 0, value, -1, buffer, static_cast<int>(size), NULL, NULL);
+            return true;
+        }
+        if (buffer && size)
+            buffer[0] = 0;
+        return false;
+    }
 
 #endif
+}
 
 #define MAKEDATE(y, m, d) (DWORD)((y << 16) + (m << 8) + d)
 
